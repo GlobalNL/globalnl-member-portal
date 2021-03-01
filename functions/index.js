12421479e8ccefda4b2a008488d515851ff83df2
Abdullah Abul Hossain <62614225+ah-abdullah@users.noreply.github.com>
2021-03-01 08:13:26 -0330

    Updating mailchimp list ID to be used as environment variable
    - Used firebase functions config to set mailchimp list ID which is now being used as a variable to set up mailchimp.
    - Slight fix on the index,js which now also updates database if a user's company name changes.

--- functions/index.js
+++ functions/index.js
@@ -62,7 +62,7 @@ exports.sendWelcomeEmail = functions.auth.user().onCreate(user => {
   var promiseArray = [];
   promiseArray.push(sendWelcomeEmail(email, displayName));
   promiseArray.push(mailchimp.post(
-			'/lists/9efe26440a/members',
+			`/lists/${functions.config().mailchimp.list_id}/members`,
 			{
 				email_address: email,
 				status: 'subscribed',
